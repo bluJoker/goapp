e7392f7d6e87bcdedef2e4533091c55a755b5ba3
wei wang <blumeng@163.com>
2022-09-06 21:39:18 +0800

    实现: player移动

--- static/js/src/playground/player/zbase.js
+++ static/js/src/playground/player/zbase.js
@@ -8,6 +8,9 @@ class Player extends AcGameObject {
 
         this.x = x;
         this.y = y;
+        //vx, vy:速度
+        this.vx = 0;
+        this.vy = 0;
         this.radius = radius;
         this.color = color;
         this.speed = speed;
@@ -15,12 +18,56 @@ class Player extends AcGameObject {
 
         // 精度:浮点运算中小于多少算0
         this.eps = 0.1;
+        this.move_length = 0;
+    }
+
+    get_dist(x1, y1, x2, y2) { // 两点间欧几里得距离
+        let dx = x1 - x2;
+        let dy = y1 - y2;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+
+    move_to(tx, ty) {
+        this.move_length = this.get_dist(this.x, this.y, tx, ty);
+        //求角度 反正切
+        let angle = Math.atan2(ty - this.y, tx - this.x);
+        //vx:横方向速度, vy:纵方向速度
+        this.vx = Math.cos(angle);
+        this.vy = Math.sin(angle);
+    }
+
+    add_listening_events() { // 鼠标点击事件
+        let outer = this;
+        this.playground.game_map.$canvas.on("contextmenu", function() { // 禁用右键显示菜单事件["contextmenu"]
+            return false;
+        });
+        this.playground.game_map.$canvas.mousedown(function(e) {
+            //3:鼠标右键, 1:左键, 2:滚轮
+            if (e.which === 3) {
+                outer.move_to(e.clientX, e.clientY);
+            }
+        });
     }
 
     start() {
+        if (this.is_me) {
+            this.add_listening_events();
+        }
     }
 
     update() {
+        if (this.move_length < this.eps) {
+            this.move_length = 0;
+            this.vx = this.vy = 0;
+        } else {
+            // 计算每帧移动距离
+            let moved = Math.min(this.move_length, this.speed * this.timedelta / 1000); // 两点间距与update时间内移动距离的小值，因为可能在update时间内就提前到达终点了
+            this.x += this.vx * moved; // 加上x方向上需要移动的距离即为此次刷新终点
+            this.y += this.vy * moved;
+            this.move_length -= moved; // 更新移动距离直到到达上面的if精度条件->表示到达鼠标终点
+        }
+
         this.render();
     }
 
