d57671e8a325ea5b8d22699e610e594cbb03418e
wei wang <blumeng@163.com>
2022-09-07 15:37:11 +0800

    实现：在menu界面前添加获取用户信息界面 & 完成用户登录注册界面的前端代码

--- static/js/src/settings/zbase.js
+++ static/js/src/settings/zbase.js
@@ -4,21 +4,140 @@ class Settings {
         this.username = "";
         this.photo = "";
 
+        this.$settings = $(`
+<div class="ac-game-settings">
+    <div class="ac-game-settings-login">
+        <div class="ac-game-settings-title">
+            登录
+        </div>
+        <div class="ac-game-settings-username">
+            <div class="ac-game-settings-item">
+                <input type="text" placeholder="用户名">
+            </div>
+        </div>
+        <div class="ac-game-settings-password">
+            <div class="ac-game-settings-item">
+                <input type="password" placeholder="密码">
+            </div>
+        </div>
+        <div class="ac-game-settings-submit">
+            <div class="ac-game-settings-item">
+                <button>登录</button>
+            </div>
+        </div>
+        <div class="ac-game-settings-error-message">
+        </div>
+        <div class="ac-game-settings-option">
+            注册
+        </div>
+        <br>
+        <div class="ac-game-settings-acwing">
+            <img width="50" src="http://118.31.14.70:8001/static/image/menu/shenle_xiao.jpg">
+            <br>
+            <div>
+                w2一键登录
+            </div>
+        </div>
+    </div>
+    <div class="ac-game-settings-register">
+        <div class="ac-game-settings-title">
+            注册
+        </div>
+        <div class="ac-game-settings-username">
+            <div class="ac-game-settings-item">
+                <input type="text" placeholder="用户名">
+            </div>
+        </div>
+        <div class="ac-game-settings-password ac-game-settings-password-first">
+            <div class="ac-game-settings-item">
+                <input type="password" placeholder="密码">
+            </div>
+        </div>
+        <div class="ac-game-settings-password ac-game-settings-password-second">
+            <div class="ac-game-settings-item">
+                <input type="password" placeholder="确认密码">
+            </div>
+        </div>
+        <div class="ac-game-settings-submit">
+            <div class="ac-game-settings-item">
+                <button>注册</button>
+            </div>
+        </div>
+        <div class="ac-game-settings-error-message">
+        </div>
+        <div class="ac-game-settings-option">
+            登录
+        </div>
+        <br>
+        <div class="ac-game-settings-acwing">
+            <img width="50" src="https://app2672.acapp.acwing.com.cn/static/image/menu/shenle_xiao.jpg">
+            <br>
+            <div>
+                w2一键登录
+            </div>
+        </div>
+
+    </div>
+</div>
+    `);
+
+        this.$login = this.$settings.find(".ac-game-settings-login");
+
+        this.$login_username = this.$login.find(".ac-game-settings-username input");
+        this.$login_password = this.$login.find(".ac-game-settings-password input");
+        this.$login_submit = this.$login.find(".ac-game-settings-submit button");
+        this.$login_error_message = this.$login.find(".ac-game-settings-error-message");
+        this.$login_register = this.$login.find(".ac-game-settings-option");
+
+        this.$login.hide();
+
+        this.$register = this.$settings.find(".ac-game-settings-register");
+        this.$register_username = this.$register.find(".ac-game-settings-username input");
+        this.$register_password = this.$register.find(".ac-game-settings-password-first input");
+        this.$register_password_confirm = this.$register.find(".ac-game-settings-password-second input");
+        this.$register_submit = this.$register.find(".ac-game-settings-submit button");
+        this.$register_error_message = this.$register.find(".ac-game-settings-error-message");
+        this.$register_login = this.$register.find(".ac-game-settings-option");
+
+        this.$register.hide();
+
+        this.root.$ac_game.append(this.$settings);
+
         this.start();
     }
 
     start() {
         this.getinfo(); // 创建时从服务器端获取用户信息
+        this.add_listening_events();
     }
 
     login() {  // 打开登录界面
-        //this.$register.hide();
-        //this.$login.show();
+        this.$register.hide();
+        this.$login.show();
     }
 
     register() {  // 打开注册界面
-        //this.$login.hide();
-        //this.$register.show();
+        this.$login.hide();
+        this.$register.show();
+    }
+
+    add_listening_events() {
+        this.add_listening_events_login();
+        this.add_listening_events_register();
+    }
+
+    add_listening_events_login() {
+        let outer = this;
+        this.$login_register.click(function() {
+            outer.register();
+        });
+    }
+
+    add_listening_events_register() {
+        let outer = this;
+        this.$register_login.click(function() {
+            outer.login();
+        });
     }
 
     getinfo() {
@@ -33,8 +152,9 @@ class Settings {
                     outer.username = resp.username;
                     outer.photo = resp.photo;
                     // 获取用户信息成功，表示已登录。将当前页面隐藏，打开菜单页面
-                    outer.hide();
-                    outer.root.menu.show();
+                    //outer.hide();
+                    //outer.root.menu.show();
+                    outer.login();
                 } else {
                     // 获取用户信息失败，打开登录界面
                     outer.login();
@@ -44,11 +164,11 @@ class Settings {
     }
 
     hide() {
-        //this.$settings.hide();
+        this.$settings.hide();
     }
 
     show() {
-        //this.$settings.show();
+        this.$settings.show();
     }
 }
 
